8a0209aa319742c77bbbd39fa78a17a619f6c0f6
Etienne Favre <etienne.favre@b-i.com>
2019-01-31 11:49:42 +0100

    Updates

---
+++ precache-manifest.b8e3f87ea1ec9bd1faa2d7ec11edd377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "336fbc5271cd68b8933a",
+    "url": "/static/js/main.336fbc52.chunk.js"
+  },
+  {
+    "revision": "4a707210739462aed1f9",
+    "url": "/static/css/1.3b380c00.chunk.css"
+  },
+  {
+    "revision": "4a707210739462aed1f9",
+    "url": "/static/js/1.4a707210.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "478cdb8a63564a6f83fe120de9cf5c04",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geo-trainer/precache-manifest.4cf28e004f642d9cc8e6e8cce3e99058.js"
+  "/precache-manifest.b8e3f87ea1ec9bd1faa2d7ec11edd377.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/geo-trainer/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
