026b8ace412f46a5676e86ba656d1eb492b84076
Etienne Favre <etienne.favre@b-i.com>
2019-01-31 11:56:59 +0100

    Updates

---
+++ geo-trainer/precache-manifest.421fee421afaee93d7e28709db1945b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e68091d44be9bd415b7d",
+    "url": "/geo-trainer/static/js/main.e68091d4.chunk.js"
+  },
+  {
+    "revision": "4a707210739462aed1f9",
+    "url": "/geo-trainer/static/css/1.3b380c00.chunk.css"
+  },
+  {
+    "revision": "4a707210739462aed1f9",
+    "url": "/geo-trainer/static/js/1.4a707210.chunk.js"
+  },
+  {
+    "revision": "31b228f3e48dd9f5d3ef",
+    "url": "/geo-trainer/static/js/runtime~main.31b228f3.js"
+  },
+  {
+    "revision": "486b4942a62ef30e194bc65b4221f3f0",
+    "url": "/geo-trainer/index.html"
+  }
+];
